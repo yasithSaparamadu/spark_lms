7973d41a8c833e476e3368dc0ee0f27253a882b2
GLSomro <yasithsaparamadu94@gmail.com>
2021-10-20 01:51:22 +0530

    Testing styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import styled from "styled-components";
+
+const Tilte = styled.h1`
+  font-sizes: 3em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 6em;
+  background: yellow;
+`;
 
 function App() {
   return (
-    <h1 style={{textAlign: "center"}}>
-      It's gonna be awsome!
-    </h1>
+    <Wrapper>
+      <Tilte>It's gonna be awsome!</Tilte>
+    </Wrapper>
   );
 }
 
