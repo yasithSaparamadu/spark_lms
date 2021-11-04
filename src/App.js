090d896679866fcda13d28012e56d73a47a1bc1c
Yasith saparamadu <yasithsaparamadu94@gmail.com>
2021-11-04 22:37:01 +0530

    Add themeing support and app bar fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { Header, Main, Footer } from "./components/Layout";
 import { NavBar, NavItem, NavLink } from "./components/Navbar";
+import { ThemeProvider } from "styled-components";
 
 function App() {
+  const theme = {
+    primary: {
+      main: "#29b6f6",
+      light: "#73e8ff",
+      dark: "#0086c3",
+      textColor: "#000"
+
+    },
+    secondary: {
+      main: "#fff",
+    }
+  };
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header>
         <NavBar>
           <NavItem href="#">
@@ -16,7 +29,7 @@ function App() {
       </Header>
       <Main>This is the main components</Main>
       <Footer>This is the footer</Footer>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,11 @@ export const NavBar = styled.ul`
   margin: 0;
   padding: 0;
   overflow: hidden;
-  background-color: #333;
+  background-color: ${(props) => props.theme.primary.main};
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
 `;
 
 export const NavItem = styled.li`
@@ -14,16 +18,16 @@ export const NavItem = styled.li`
 
 export const NavLink = styled.a`
   display: block;
-  color: white;
+  color: ${(props) => props.theme.primary.textColor};
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
 
   :hover {
-    background-color: #4fc3f7;
+    background-color: #${(props) => props.theme.primary.light};
   }
 
   .active {
-      background-color: 304aa6d;
+      background-color: ${(props) => props.theme.primary.dark};
   }
 `;
